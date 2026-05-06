bee339427968928c781a06c8670bc6197a80bf5f
skellarau <skellar.au@gmail.com>
2026-05-06 17:41:21 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
 const CACHE_NAME = 'pool-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// Install and cache the app shell
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
